d1577356367d854255e905e1d412be4feba4f0da
Mayankyadav3980 <ymayank501@gmail.com>
2024-12-07 13:35:34 +0530

    add spinner

--- src/components/AlbumList/AlbumList.js
+++ src/components/AlbumList/AlbumList.js
@@ -5,14 +5,16 @@ import { onSnapshot, collection, orderBy } from "firebase/firestore";
 import  Card  from '../Card/Card'
 import AlbumForm from "../AlbumForm/AlbumForm";
 import ImageList from "../ImageList/ImageList";
+import Spinner from "react-spinner-material";
 
 const AlbumList = () => {
     const [albumList, setAlbumList] = useState([]);
     const [showForm, setShowForm] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [openAlbum, setOpenAlbum] = useState({
       // albumId: "5FTASPJYo4JkgjZharZG",
       albumId: "6eXkPuY09N1Sjg9CsKFT",
-      open: true,
+      open: false,
     });
 
     useEffect(()=>{
@@ -24,41 +26,54 @@ const AlbumList = () => {
           }
         })
         setAlbumList(list);
+        setLoading(false);
       })
 
     },[])
 
     return (
-      <div className={styles.main_container}>
-        {!openAlbum.open ? (
-          <>
-            {!showForm && (
-              <div className={styles.btndiv}>
-                <button
-                  className={styles.btn}
-                  onClick={() => setShowForm(!showForm)}
-                >
-                  Add Album
-                </button>
-              </div>
+      <>
+        {loading ? (
+          <div className={styles.spinner_cont}>
+            <Spinner visible={true} radius={60} color={"black"} stroke={5} />
+          </div>
+        ) : (
+          <div className={styles.main_container}>
+            {!openAlbum.open ? (
+              <>
+                {!showForm && (
+                  <div className={styles.btndiv}>
+                    <button
+                      className={styles.btn}
+                      onClick={() => setShowForm(!showForm)}
+                    >
+                      Add Album
+                    </button>
+                  </div>
+                )}
+
+                {showForm && <AlbumForm />}
+                <div className={styles.container}>
+                  <h2 className={styles.heading}>Your Albums</h2>
+                  <div className={styles.album_list}>
+                    {albumList.map((album, idx) => {
+                      return (
+                        <Card
+                          key={idx}
+                          album={album}
+                          setOpenAlbum={setOpenAlbum}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <ImageList openAlbum={openAlbum} setOpenAlbum={setOpenAlbum} />
             )}
-
-            {showForm && <AlbumForm />}
-            <div className={styles.container}>
-              <h2 className={styles.heading}>Your Albums</h2>
-              <div className={styles.album_list}>
-
-                {albumList.map((album, idx) => {
-                  return (
-                    <Card key={idx} album={album} setOpenAlbum={setOpenAlbum} />
-                  );
-                })}
-              </div>
-            </div>
-          </>
-        ) : (<ImageList openAlbum={openAlbum} setOpenAlbum={setOpenAlbum} />)
-        }
-      </div>
+          </div>
+        )}
+      </>
     );
 }
 
